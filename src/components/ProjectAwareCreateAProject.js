f8df1d8b7ca0e6ca65bdb85f7e7e5de0626b6254
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2021-06-09 16:22:19 +0200

    MFLT-3637: Improve nRF SDK docs: include tabs depending on SDK version (#66)
    * MFLT-3637: Improve nRF SDK docs: include tabs depending on SDK version
    
    Our nRF Connect SDK guide needed an update for the upcoming SDK version
    1.6.0 by Nordic, which [ships with built-in support for Memfault](https://github.com/nrfconnect/sdk-nrf/pull/4670).
    
    It has not been released yet, so maybe it's necessary to hold back on
    merging this until then.
    
    Additionally, I've made some other changes:
    
    - Generally reduced drama by changing `:::caution` to `:::important`
      (cautions are orange, "importants" are blue).
    - Added a `ProjectAwareCreateAProject` component that will either
      disappear if in `/embed` mode, show the project key if it's available,
      or show what we showed before otherwise ("create a project and get a
      key").
    
    * Prettified Code!
    
    * Change documented memfault SDK location to the new /modules/lib
    
    * Upgrade Docusaurus from 2.0.0-alpha.70 to to 2.0.0-beta.0
    
    It enables the use of explicit heading IDs: https://docusaurus.io/docs/markdown-features/headings#explicit-ids
    
    These are also in the docs for older versions but don't actually work.
    
    * Build with newer Node 12.22.1
    
    * Hide nRF Connect SDK spoilers
    
    Co-authored-by: fnune <fnune@users.noreply.github.com>

---
+++ src/components/ProjectAwareCreateAProject.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import { useLocation } from "react-router";
+
+import ProjectSetupDetailsContext from "./ProjectSetupDetailsContext";
+
+export default function ProjectAwareCreateAProject() {
+    const [{ projectKey }] = useContext(ProjectSetupDetailsContext);
+    const { pathname } = useLocation();
+
+    if (pathname?.startsWith("/embed")) {
+        // We're already in a project. The user doesn't need to create one.
+        return null;
+    }
+
+    if (projectKey)
+        return (
+            <>
+                <h3>Copy your Project Key</h3>
+                <p>
+                    You already have a project, and your project key is{" "}
+                    <code>{projectKey}</code>.
+                </p>
+            </>
+        );
+
+    return (
+        <>
+            <h3>Create a Project and get a Project Key</h3>
+            <p>
+                Go to{" "}
+                <a
+                    href="https://app.memfault.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    app.memfault.com
+                </a>{" "}
+                and from the "Select A Project" dropdown, click on "Create
+                Project" to setup your first project such as "smart-sink-dev".
+            </p>
+            <p>
+                Once you've created your project, you'll be automatically taken
+                to an integration guide which includes your project key. Copy it
+                to follow the rest of the guide.
+            </p>
+        </>
+    );
+}
