3c86f00ca2b1964721eb1219ebbfcfa571c3b4b8
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2022-01-12 17:22:14 +0100

    MFLT-5058 Document the version matrix in the platform OTA doc (#148)
    * MFLT-5058 Document the version matrix in the platform OTA doc 
    
     Reviewed by Marcel and Heiko. Thanks!

--- src/components/icons/TargetVersionFilled.js
+++ src/components/icons/TargetVersionFilled.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import useThemeContext from "@theme/hooks/useThemeContext";
+
 export default function TargetVersionFilled() {
+    const { isDarkTheme } = useThemeContext();
+    const color = isDarkTheme ? "#fff" : "#000";
     return (
         <svg
             width="1em"
@@ -16,11 +20,11 @@ export default function TargetVersionFilled() {
                 cx="32"
                 cy="32"
                 r="29.5"
-                stroke="#000"
+                stroke={color}
                 fill="transparent"
                 stroke-width="5"
             ></circle>
-            <circle cx="32" cy="32" r="17" fill="#000"></circle>
+            <circle cx="32" cy="32" r="17" fill={color}></circle>
         </svg>
     );
 }
