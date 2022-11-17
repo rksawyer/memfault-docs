fefaab602d98adde06d9dda1d90c59a24451c5b8
can-memfault <108698136+can-memfault@users.noreply.github.com>
2022-11-17 12:09:21 +0100

    Several typo fixes and improvements of the Android architecture images (#336)
    * Several typo fixes and improvements to the images
    
    * Prettified Code!
    
    * Update docs/android/android-getting-started-guide.mdx
    
    Co-authored-by: Heiko Behrens <mail@HeikoBehrens.net>
    
    * Prettified Code!
    
    * Update docs/android/android-getting-started-guide.mdx
    
    Co-authored-by: Fausto Núñez Alberro <fausto.nunez@mailbox.org>
    
    * Add link to the quotas docs page
    
    * Prettified Code!
    
    * use SVGs instead of PNGs for Android diagrams
    
    Co-authored-by: can-memfault <can-memfault@users.noreply.github.com>
    Co-authored-by: Heiko Behrens <mail@HeikoBehrens.net>
    Co-authored-by: Fausto Núñez Alberro <fausto.nunez@mailbox.org>

--- sidebars.js
+++ sidebars.js
@@ -127,7 +127,7 @@ module.exports = {
                 "android/android-releases-integration-guide",
                 "android/android-ota-update-client",
                 "android/android-data-scrubbing",
-                // "android/android-multi-device-support",
+                "android/android-multi-device-support",
             ],
         },
         {
