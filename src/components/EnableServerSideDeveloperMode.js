50c1682d90caa3e0238f94e86d2f5879d81b995d
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2023-05-08 12:39:19 +0200

    MFLT-9887 Use "Developer Mode" instead of other names (#462)
    * MFLT-9887 Use "Developer Mode" instead of other names
    
     Matching link changes have been done on the app side and will be
     shipped soon.
    
    * Rename component use also
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: fnune <fnune@users.noreply.github.com>

--- src/components/EnableServerSideDeveloperMode.js
+++ src/components/EnableServerSideDeveloperMode.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export function EnableServerSideDevelopmentMode() {
+export function EnableServerSideDeveloperMode() {
     return (
         <>
             Server-side rate limiting will apply to the device you're using to
             work on the integration process. Once you can see the device on the
             Memfault Web App, consider enabling{" "}
-            <a href="/docs/platform/rate-limiting#server-side-development-mode">
-                Server-Side Development Mode
+            <a href="/docs/platform/rate-limiting#server-side-developer-mode">
+                Server-Side Developer Mode
             </a>{" "}
             for it on the Memfault Web App to temporarily bypass these limits.
         </>
